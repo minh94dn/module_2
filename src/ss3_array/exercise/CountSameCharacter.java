258739e26850eb7d0201d2a58a70a4f2f992fb24
minh94dn <grakk2405@gmail.com>
2022-10-07 15:34:06 +0700

    ss2 và ss3 nop bai tap

--- src/ss3_array/exercise/CountSameCharacter.java
+++ src/ss3_array/exercise/CountSameCharacter.java
@@ -1,4 +1,23 @@
 package ss3_array.exercise;
 
+import java.util.Scanner;
+
 public class CountSameCharacter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập ký tự trong chuỗi: ");
+        String str = scanner.nextLine();
+        System.out.print("Nhập ký tự cần kiểm tra: ");
+        char character = scanner.nextLine().charAt(0);
+        int count = 0;
+        int index = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (character == str.charAt(i)) {
+                count++;
+                index = i;
+                System.out.println("Vị trị xuất hiện " + character + " trong chuỗi là: " + i);
+            }
+        }
+        System.out.print("Số ký tự " + character + " xuất hiện ở trong chuỗi là: " + count);
+    }
 }
